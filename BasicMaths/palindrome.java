782255a23c0aaf10df172f31bbe0fb374fdaaf43
Madhu <madhuanbarasu19@gmail.com>
2026-08-13 23:21:06 +0530

    Palindrome number check

---
+++ BasicMaths/palindrome.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int org = num;
+        int rev = 0;
+        while (num > 0) {
+            int dig = num % 10;
+            rev = (rev * 10) + dig;
+            num = num / 10;
+        }
+        if (org== rev) {
+            System.out.print("true");
+        } else {
+            System.out.print("false");
+        }
+    }
+}
+
+
+
