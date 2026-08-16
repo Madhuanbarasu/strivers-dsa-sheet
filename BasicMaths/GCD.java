c153a3c4311645f1596a94e3b6c1cbe96aadbebc
Madhu <madhuanbarasu19@gmail.com>
2026-08-16 19:21:39 +0530

    Gcd of two numbers

---
+++ BasicMaths/GCD.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class GCD {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        int gcd=0;
+
+        for(int i=1;i<=Math.min(n1,n2);i++){
+            if(n1%i==0&&n2%i==0){
+                 gcd=i;
+            }
+        }
+        System.out.print(gcd);
+
+    }
+}
