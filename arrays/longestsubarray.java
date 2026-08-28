e7a1ab86ea7e7abe16eca0393167dd09eb34baab
Madhu <madhuanbarasu19@gmail.com>
2026-08-28 23:08:02 +0530

    longest subarray with sum K

---
+++ arrays/longestsubarray.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class longestsubarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        int len = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum == k) {
+                    len = Math.max(len, j - i + 1);
+                }
+
+            }
+        }
+        System.out.print(len);
+
+
+    }
+
+}
