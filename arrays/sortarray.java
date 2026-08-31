a57f72519cdb636b298b24ab01c9ba32aa746b7a
Madhu <madhuanbarasu19@gmail.com>
2026-08-31 09:40:24 +0530

    sort an array of 0s 1s and 2s

---
+++ arrays/sortarray.java
@@ -0,0 +1,38 @@
+
+
+import java.util.Scanner;
+public class sortarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                count0++;
+            } else if (arr[i] == 1) {
+                count1++;
+            } else {
+                count2++;
+            }
+        }
+        for (int i = 0; i < count0; i++) {
+            arr[i] = 0;
+        }
+        for (int i = count0; i < count0 + count1; i++) {
+            arr[i] = 1;
+        }
+        for (int i = count0 + count1; i < n ; i++) {
+            arr[i] = 2;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
