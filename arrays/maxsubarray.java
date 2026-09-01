e6673b0a27a0114900aa54d5b3ca1bc5a62e5db3
Madhu <madhuanbarasu19@gmail.com>
2026-09-01 12:05:48 +0530

    Maximum subarray sum using kadane's algorithm

---
+++ arrays/maxsubarray.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class maxsubarray {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            max=Math.max(sum,max);
+            if(sum<0) sum=0;
+
+        }
+        System.out.print(max);
+    }
+}
