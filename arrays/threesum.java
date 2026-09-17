478bc134d228c7cfbf2d32b2331e7479bbfe39a9
Madhu <madhuanbarasu19@gmail.com>
2026-09-17 22:48:21 +0530

    3 sum

---
+++ arrays/threesum.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class threesum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(arr[i]);
+                        temp.add(arr[j]);
+                        temp.add(arr[k]);
+                        Collections.sort(temp);
+                        if(!ans.contains(temp)){
+                            ans.add(temp);
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(ans);
+
+    }
+}
