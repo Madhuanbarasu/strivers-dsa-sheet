a5989892e9304bdaa9c60c45b03325e23d13ac0c
Madhu <madhuanbarasu19@gmail.com>
2026-09-19 22:48:07 +0530

    four sum

---
+++ arrays/foursum.java
@@ -0,0 +1,59 @@
+import java.util.*;
+public class foursum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target=sc.nextInt();
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(arr);
+        for(int i=0;i<n;i++){
+            if(i>0 && arr[i]==arr[i-1]){
+                continue;
+            }
+            for(int j=i+1;j<n;j++){
+                if(j!=i+1 && arr[j]==arr[j-1]){
+                    continue;
+                }
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long sum =(long) arr[i];
+                    sum+=arr[j];
+                    sum+=arr[k];
+                    sum+=arr[l];
+                    if(sum==target){
+                        List<Integer> temp=new ArrayList<>();
+                        temp.add(arr[i]);
+                        temp.add(arr[j]);
+                        temp.add(arr[k]);
+                        temp.add(arr[l]);
+                        ans.add(temp);
+                        k++;
+                        l--;
+                        while(k<l && arr[k]==arr[k-1]){
+                            k++;
+                        }
+                        while(k<l && arr[l]==arr[l+1]){
+                            l--;
+                        }
+                    }
+                    else if(sum<target){
+                        k++;
+                    }
+                    else{
+                        l--;
+                    }
+
+                }
+
+            }
+        }
+    System.out.println(ans);
+
+    }
+}
